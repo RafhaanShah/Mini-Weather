0aaf23082ed3302e079ad93182b752419c0f2e58
Raf <RafhaanShah@users.noreply.github.com>
2020-10-10 10:56:12 +0100

    Fix Glide warning

--- app/src/main/java/com/miniweather/service/ImageService.kt
+++ app/src/main/java/com/miniweather/service/ImageService.kt
@@ -3,7 +3,12 @@ package com.miniweather.service
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import com.miniweather.R
+
+@GlideModule
+class GlideModule : AppGlideModule()
 
 class ImageService {
 
